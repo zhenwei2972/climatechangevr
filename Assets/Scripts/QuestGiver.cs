dac65b08e2b092b6cfbf5756478759df1a2a7858
KohTongLiang <koh.tongliang@gmail.com>
2022-02-10 15:51:26 +0800

    Added quest giver system. Added Test scene.

---
+++ Assets/Scripts/QuestGiver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class QuestGiver : MonoBehaviour
+{
+    public Quest[] quest;
+    public int currentQuest = 0;
+    public Player player;
+
+    // initiate quest, set isActive to true
+    public void StartQuest()
+    {
+        quest[currentQuest].isActive = true;
+    }
+    public void EndQuest()
+    {
+        quest[currentQuest].isActive = false;
+    }
+    // retrieve quest object
+    public Quest GetQuestInfo()
+    {
+        return quest[currentQuest];
+    }
+    public void NextQuest()
+    {
+        currentQuest++;
+    }
+}

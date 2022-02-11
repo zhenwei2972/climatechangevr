867c9104132f3e6efb07c5b9b374ae9678139a1a
#LEE ZHEN WEI# <ZLEE041@student.main.ntu.edu.sg>
2022-02-11 17:27:30 +0800

    new assets

---
+++ Assets/Invert.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class Invert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public MeshCollider meshCollider;
+
+    private void Awake()
+    {
+        if (!meshCollider) meshCollider = GetComponent<MeshCollider>();
+
+        var mesh = meshCollider.sharedMesh;
+
+        // Reverse the triangles
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+
+        // also invert the normals
+        mesh.normals = mesh.normals.Select(n => -n).ToArray();
+    }
+}

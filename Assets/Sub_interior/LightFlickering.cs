a31339ccd767e666d4ef9f04b2ec47011eeeb3df
Yong Qiang <chienyq221@gmail.com>
2022-02-16 16:22:02 +0800

    lvl-2 sub
    Delon - Submarine level with water, without event

---
+++ Assets/Sub_interior/LightFlickering.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickering : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+    public Light lightFlicker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        lightFlicker.intensity = 0;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        lightFlicker.intensity = 1.4f;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
---
+++ Assets/Sub_interior/LightingBreathing.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingBreathing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Light lightToFade;
+    public float eachFadeTime = 2f;
+    public float fadeWaitTime = 0;
+
+    public float minLux = 0;
+    public float maxLux = 1.6f;
+    
+    void Awake()
+    {
+        StartCoroutine(fadeInAndOutRepeat(lightToFade, eachFadeTime, fadeWaitTime));
+    }
+    IEnumerator fadeInAndOut(Light lightToFade, bool fadeIn, float duration)
+    {
+        float minLuminosity = 0; // min intensity
+        float maxLuminosity = 1.4f; // max intensity
+
+        float counter = 0f;
+
+        //Set Values depending on if fadeIn or fadeOut
+        float a, b;
+
+        if (fadeIn)
+        {
+            a = minLuminosity;
+            b = maxLuminosity;
+        }
+        else
+        {
+            a = maxLuminosity;
+            b = minLuminosity;
+        }
+
+        float currentIntensity = lightToFade.intensity;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+
+            lightToFade.intensity = Mathf.Lerp(a, b, counter / duration);
+
+            yield return null;
+        }
+    }
+    IEnumerator fadeInAndOutRepeat(Light lightToFade, float duration, float waitTime)
+    {
+        WaitForSeconds waitForXSec = new WaitForSeconds(waitTime);
+
+        while (true)
+        {
+            //Fade out
+            yield return fadeInAndOut(lightToFade, false, duration);
+
+            //Wait
+            //yield return waitForXSec;
+
+            //Fade-in 
+            yield return fadeInAndOut(lightToFade, true, duration);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

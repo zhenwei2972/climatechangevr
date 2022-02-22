09d2e2857c822d9c881a4d6c98af3d75426e5a90
MasterWong382 <masterwong382@gmail.com>
2022-02-22 13:59:32 +0800

    EndGameScene + Audio
    end game scene

---
+++ Assets/Level1Only/ScriptsForLvl1/EndGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject smoke;
+    public AudioSource gasSound;
+
+    void Start()
+    {
+        StartCoroutine("Smoke");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If Press A
+        SceneManager.LoadScene("");
+    }
+
+    IEnumerator Smoke()
+    {
+        yield return new WaitForSeconds(8f);
+        smoke.SetActive(true);
+        gasSound.Play();
+    }
+}

a826ec1b7591e0e80f803c9a57852f38bc8eaa11
DelonLim <delonlim1994@gmail.com>
2022-02-27 08:40:06 +0900

    Submarine to EndCredits Scene added collider to prevent glitching in the scene
    Submarine to EndCredits Scene added, added collider to prevent glitching in the scene

---
+++ Assets/Scripts/TaskComplete.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TaskComplete : MonoBehaviour
+{
+    public GameObject ArcEngine;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(ArcEngine))
+        {
+            Debug.Log("Game End");
+            SceneManager.LoadScene("FinalSceneEndGame");
+        }
+    }
+}

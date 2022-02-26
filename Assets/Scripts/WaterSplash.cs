21ef020d3891dd250ce4e097986f12997c4a838c
DelonLim <delonlim1994@gmail.com>
2022-02-27 07:51:42 +0900

    Scripted watersplash, remove bug
    Scripted water splash, remove bug of bench moving to side due to rigidbody

---
+++ Assets/Scripts/WaterSplash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour
+{
+    public GameObject Chair1, Chair2, Rock;
+    public ParticleSystem waterSplash;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(Chair1) || other.gameObject.Equals(Chair2) || other.gameObject.Equals(Rock))
+        {
+            waterSplash.Play();
+        }
+    }
+
+}

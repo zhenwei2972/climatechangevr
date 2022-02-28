55f9fcda0cfc50fce0f7a52cbb355b38e951e50d
DelonLim <delonlim1994@gmail.com>
2022-02-28 09:00:08 +0900

    Add splash audio
    Once rock and chair fall into water will have splash particles and audio play

--- Assets/Scripts/WaterSplash.cs
+++ Assets/Scripts/WaterSplash.cs
@@ -6,12 +6,14 @@ public class WaterSplash : MonoBehaviour
 {
     public GameObject Chair1, Chair2, Rock;
     public ParticleSystem waterSplash;
+    public AudioSource SplashSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.Equals(Chair1) || other.gameObject.Equals(Chair2) || other.gameObject.Equals(Rock))
         {
             waterSplash.Play();
+            SplashSound.Play();
         }
     }
 
